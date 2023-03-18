5480d36c2856d49a4d6088db8a6561fb5c3a661c
farahahmeddeen <farahahmeddeen1995@gmial.com>
2023-03-18 03:20:48 -0700

    possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int z;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			for (z = 2 ; z < 10 ; z++)
+			{
+				if (i < j && j < z)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(z + '0');
+					if (i + j + z != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
