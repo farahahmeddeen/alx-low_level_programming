59eb2fc5b013aed83820ce55753aeb7fe570050a
farahahmeddeen <farahahmeddeen1995@gmai.com>
2023-03-21 04:34:14 -0700

    function that prints alphabet in lowecase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char k;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (k = 'a'; k <= 'z' ; k++)
+			_putchar(k);
+		_putchar('\n');
+	}
+}
+
