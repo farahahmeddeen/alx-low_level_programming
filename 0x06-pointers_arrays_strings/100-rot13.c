f4c8882230b7da0cc30a0639f6a9641b7c5f5c35
farahahmeddeen <farahahmeddeen1995@gmai.com>
2023-03-30 02:51:42 -0700

    function that replaces a letter with the 13th letter after it

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * rot13 - function thatreplaces the 13th letter
+ *
+ * @y: pointer
+ *
+ * Return: to y
+ */
+
+char *rot13(char *y)
+{
+	int i;
+	int j;
+	char ori[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z'};
+	char rotate[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M'};
+
+	for (i = 0; y[i] != '\0' ; i++)
+	{
+		for (j = 0 ; ori[j] != '\0' ; j++)
+		{
+			if (y[i] == ori[j])
+				y[i] = rotate[j];
+		}
+	}
+	return (y);
+}
