3bbd45b72fc00eddd5bb2af648eaa7fa6aaf6e95
farahahmeddeen <farahahmeddeen1995@gmail.com>
2023-05-08 02:56:47 -0700

    function that reads a text file and prints it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile - reads texts and prints it
+ *
+ * @filename: name of the file
+ * @letters: number of letters
+ *
+ * Return: letters it can read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fh;
+	ssize_t rd, wr;
+	char *fa;
+
+	if (filename)
+		return (1);
+	else
+		return (0);
+	fh = open(filename, O_RDONLY);
+	if (fh == -1)
+		return (0);
+
+	fa = malloc(sizeof(char) * (letters));
+
+	if (fa == NULL)
+		return (0);
+	rd = read(fh, fa, letters);
+	wr = write(STDOUT_FILENO, fa, rd);
+	close(fh);
+	free(fa);
+
+	return (wr);
+}
